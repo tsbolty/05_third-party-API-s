d87d70dcb530a176fe25130c80fd06a37af1bfc6
Tyler Bolty <tsbolty@gmail.com>
2020-10-13 21:21:08 -0600

    refactored using template literals

--- logic.js
+++ logic.js
@@ -4,22 +4,16 @@ var hour = [9, 10, 11, 12, 13, 14, 15, 16, 17]
 var displayHour= ["9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM"]
 
 for (i=0; i<hour.length; i++){
-    var createTextarea= $("<textarea>")
-    var createDiv = ($("<div>").attr("id",'hour-'+hour[i]).attr("class", "row time-block").attr("value", hour[i]))
-    $(".container").append(createDiv)
-    
-    
-    if (hour[i]<12){
-        var currentTime = displayHour[i]
-    } else if(hour[i]>11){
-        var currentTime = displayHour[i]    
-    }
 
-    createDiv.append($("<div class= col-md-1 hour>").text(currentTime))
-    createDiv.append($("<textarea class= 'col-md-10 description' id= 'memo-area'>"))
-    
-    createDiv.append($("<button class='button saveBtn col-md-1'>"))
-    $("button").text("Save")
+    var currentTime = displayHour[i]
+
+    $(".container").append(`
+      <div id= "hour-${hour[i]}" class= "row time-block">
+        <div class= "col-md-1 hour">${currentTime}</div>
+        <textarea class= "col-md-10 description" id= "memo-area" />
+        <button class= "button saveBtn col-md-1">Save</button>
+      </div>
+    `)
 
     if(moment().format("H") == hour[i]){
     $("#hour-"+ hour[i]).addClass("present")
@@ -27,15 +21,14 @@ for (i=0; i<hour.length; i++){
     $("#hour-"+ hour[i]).addClass("past")
     } else if(moment().format("H")< hour[i]){
     $("#hour-"+ hour[i]).addClass("future")
-}
-}
-
+}}
 
 $(".saveBtn").on("click", function(){
     var value= $(this).siblings(".description").val()
     var time= $(this).parent().attr("id")
     localStorage.setItem(time, value)
 })
+
 $("#hour-9 .description").val(localStorage.getItem("hour-9"))
 $("#hour-10 .description").val(localStorage.getItem("hour-10"))
 $("#hour-11 .description").val(localStorage.getItem("hour-11"))
